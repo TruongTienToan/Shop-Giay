e3f3c4887b9b4084d20942c5c43240051ee950ce
TruongTienToan <truongtientoan2308@gmail.com>
2022-10-07 09:44:30 +0700

    case-study

--- case1c.js
+++ case1c.js
@@ -1,7 +1,7 @@
-let shoes = new Shoes('01', "Adidas Xspeedportal", '38-42', 'Trắng Cam',3000000,'https://o.remove.bg/downloads/f4269453-96a4-4047-8fc0-286ee41ef519/image-removebg-preview.png' );
-let shoes1 = new Shoes('02','Adidas Predator','36-42','Trắng', 2700000,'https://o.remove.bg/downloads/55b32b7a-2d02-4c8b-81fa-fca9870998ec/image-removebg-preview.png');
-let shoes2 = new Shoes('03','Adidas Predator','38-42','Đỏ',2900000,'https://o.remove.bg/downloads/b4cf228a-00f8-40cc-8d43-e1d086af52e8/image-removebg-preview.png');
-let shoes3 = new Shoes('04','Adidas Xspeedportal','39-42','Xanh lá',3500000,'https://o.remove.bg/downloads/5f91e448-c1e8-4814-8865-b91d09caf32e/image-removebg-preview.png');
+let shoes = new Shoes('01', "Adidas Xspeedportal", '38-42', 'Trắng Cam',3000000,'./assets/images/1.png');
+let shoes1 = new Shoes('02','Adidas Predator','36-42','Trắng', 2700000,'./assets/images/2.png');
+let shoes2 = new Shoes('03','Adidas Predator','38-42','Đỏ',2900000,'./assets/images/3.png');
+let shoes3 = new Shoes('04','Adidas Xspeedportal','39-42','Xanh lá',3500000,'./assets/images/4.png');
 let arr =[shoes, shoes1, shoes2, shoes3];
 let manage = new shoesManagement(arr);
 
